fda53ed4019f8a8b7447fceab5be0ece954e6983
Gremlin <liveylou@gmail.com>
2024-07-02 18:11:42 -0400

    adds things for kinematics

--- src/main/java/frc/robot/Subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/DriveSubsystem.java
@@ -3,9 +3,11 @@ package frc.robot.Subsystems;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
@@ -13,18 +15,41 @@ import com.ctre.phoenix6.hardware.Pigeon2;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.ReplanningConfig;
 
+import com.revrobotics.RelativeEncoder;
+
 public class DriveSubsystem extends SubsystemBase {
    
    private final WPI_VictorSPX m_left;
    private final WPI_VictorSPX m_right;
-   
-    private final Pigeon2 m_gyro = new Pigeon2(0);
 
+    private final Pigeon2 m_gyro = new Pigeon2(0);
+    private final Encoder m_leftEncoder;
+    private final Encoder m_rightEncoder;
+
+    
     private final DifferentialDriveOdometry m_odometry;
-public DriveSubsystem(WPI_VictorSPX left, WPI_VictorSPX right){
+    private final DifferentialDriveKinematicas m_kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(20.75));
+
+    public DriveSubsystem(WPI_VictorSPX left, WPI_VictorSPX right){
     this.m_left = left;
     this.m_right = right;
-    m_odometry= new DifferentialDriveOdometry(new Rotation2d(),0,0);
+    m_odometry= new DifferentialDriveOdometry(
+      new Rotation2d(),
+      this.m_leftEncoder.getDistance(),
+      this.m_rightEncoder.getDistance(),
+      new Pose2d(x:1.0, y:2.0, new Rotation2d())
+    );
+@Override
+public void periodic() {
+  // Get the rotation of the robot from the gyro.
+  var gyroAngle = m_gyro.getRotation2d();
+
+  // Update the pose
+  m_pose = m_odometry.update(gyroAngle,
+    m_leftEncoder.getDistance(),
+    m_rightEncoder.getDistance());
+}
+    
 
 
     
@@ -52,7 +77,7 @@ public Pose2d getPose() {
     return m_odometry.getPoseMeters();
 }
 public void resetPose(Pose2d pose) {
-    m_odometry.resetPosition(m_gyro.getRotation2d(), null, pose);
+    m_odometry.resetPosition(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), pose);
 }
 
 public ChassisSpeeds getCurrentSpeeds() {
@@ -60,6 +85,9 @@ public ChassisSpeeds getCurrentSpeeds() {
 }
 
 public void drive(ChassisSpeeds speeds) {
+DifferentailDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(chassisSpeeds);
+m_left.set(wheelSpeeds.leftMetersPerSecond); //TODO convert convert m/s voltage
+m_right.set(wheelSpeeds.rightMetersPerSecond);
 
 }
 
